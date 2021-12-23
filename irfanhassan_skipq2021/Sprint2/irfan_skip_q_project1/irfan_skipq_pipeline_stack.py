17170ce4b8983b1b19875685297bc1a01f58aa89
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-23 19:24:06 +0000

    updated

--- irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
+++ irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
@@ -20,6 +20,6 @@ class IrfanSkipQPpielineStack(cdk.Stack):
                             primary_output_directory = "irfanhassan_skipq2021/Sprint2/cdk.out"
                             )
         pipeline = pipelines.CodePipeline(self,'pipeline',synth=synth)
-        #beta_stage = IrfanSkipQProject1Stack(self, "Beta_stage", env = { 'account': '315997497220', 'region': 'us-east-2'})
-        #pipeline.add_stage(beta_stage)
+        beta_stage = IrfanSkipQProject1Stack(self, "Beta_stage", env = { 'account': '315997497220', 'region': 'us-east-2'})
+        pipeline.add_stage(beta_stage)
         
