987d61b8258d726ef0138c8a034273a4ecd8b24c
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-24 11:54:23 +0000

    updated

--- irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skip_q_project1_stage.py
+++ irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skip_q_project1_stage.py
@@ -6,4 +6,4 @@ class IrfanSkipQPpielineStage(cdk.Stage):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
-        irfan_stack =  IrfanSkipQProject1Stack(self,'IrfanSkipQProject1Stack')
+        irfan_stack =  IrfanSkipQProject1Stack(self,'irfan_stack')
