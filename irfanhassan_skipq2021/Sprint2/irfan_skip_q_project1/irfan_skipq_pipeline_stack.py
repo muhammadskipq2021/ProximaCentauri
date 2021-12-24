4d387a70738120712d9b82a3c74d12d33befcd1d
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-24 16:21:38 +0000

    updated

--- irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
+++ irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
@@ -20,7 +20,7 @@ class IrfanSkipQPpielineStack(cdk.Stack):
                             primary_output_directory = "irfanhassan_skipq2021/Sprint2/cdk.out"
                             )
         pipeline = pipelines.CodePipeline(self,'pipeline',synth=synth)
-        beta_stage = IrfanSkipQPpielineStage(self, "Beta_stage", env = { 'account': '315997497220', 'region': 'us-east-2'})
+        betaStage = IrfanSkipQPpielineStage(self, "BetaStage", env = { 'account': '315997497220', 'region': 'us-east-2'})
         #test = pipeline.ShellStep('unit_test',"cd irfanhassan_skipq2021/Sprint2", "pip install -r requirements.txt",
         #"pip install pytest", "pytest unitTest","pytest intigrationTest")
         #pipeline.add_stage(beta_stage)
