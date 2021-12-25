869a8e78fb6459d2a3eeed61cd6c9ddd3355dbfd
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-25 18:57:51 +0000

    updated

--- irfanhassan_skipq2021/Sprint2_irfan/sprint2_irfan/sprint2_irfan_stack.py
+++ irfanhassan_skipq2021/Sprint2_irfan/sprint2_irfan/sprint2_irfan_stack.py
@@ -29,7 +29,7 @@ class Sprint2IrfanStack(cdk.Stack):
         our_rule = event_.Rule(self, id = "MonitorwebHealth",enabled = True, schedule= lambda_schedule,targets =[lambda_target])
                 
         #creating dynamodb table 
-        dynamo_table=self.create_table(id='irfanhassantable',name = constant_.table_name, key=db.Attribute(name="Timestamp", type=db.AttributeType.STRING))
+        dynamo_table=self.create_table(id='irfanhassantable', key=db.Attribute(name="Timestamp", type=db.AttributeType.STRING))
         db_lambda_role = self.create_db_lambda_role()
         db_lamda = self.create_lambda('secondHellammbda',"./resources/",'dynamodb_lambda.lambda_handler',db_lambda_role)
         dynamo_table.grant_full_access(db_lamda)
@@ -119,9 +119,8 @@ class Sprint2IrfanStack(cdk.Stack):
                         ])
         return lambdaRole
 #creating dynamo table 
-    def create_table(self,id,name,key):
+    def create_table(self,id,key):
         return db.Table(self,id,
-        table_name = name,
         partition_key=key)
     
     
