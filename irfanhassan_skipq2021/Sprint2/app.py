7ca8346afe3662cc231fdf826bbca13b2d462669
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-25 15:54:13 +0000

    irfan

--- irfanhassan_skipq2021/Sprint2/app.py
+++ irfanhassan_skipq2021/Sprint2/app.py
@@ -13,7 +13,7 @@ from aws_cdk import core
 from irfan_skip_q_project1.irfan_skipq_pipeline_stack import IrfanSkipqPipelineStack
 
 app = core.App()
-IrfanSkipqPipelineStack(app, "IrfanSkipqPipelineStack1",env=core.Environment(account='315997497220', region='us-east-2'))
+IrfanSkipqPipelineStack(app, "IrfanSkipqPipelineStack51",env=core.Environment(account='315997497220', region='us-east-2'))
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
