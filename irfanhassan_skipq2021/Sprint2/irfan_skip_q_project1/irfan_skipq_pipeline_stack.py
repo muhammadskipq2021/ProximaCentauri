a7713cdcc06e4c21de72497d342174afab66243a
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-25 15:40:02 +0000

    updated

--- irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
+++ irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skipq_pipeline_stack.py
@@ -1,21 +1,21 @@
 from aws_cdk import (
-    core as cdk,
+    core,
     pipelines,
     aws_codepipeline_actions as cpactions
 )
 from irfan_skip_q_project1.irfan_skip_q_project1_stage import IrfanSkipqPipelineStage
 
-class IrfanSkipqPipelineStack(cdk.Stack):
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+class IrfanSkipqPipelineStack(core.Stack):
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
         ###### source ########################
         source = pipelines.CodePipelineSource.git_hub(repo_string = "muhammadskipq2021/ProximaCentauri",branch = "main",
-                           authentication = cdk.SecretValue.secrets_manager("Irfan_sprint2_secretkey"),
+                           authentication = core.SecretValue.secrets_manager("Irfan_sprint2_secretkey"),
                            trigger = cpactions.GitHubTrigger.POLL)
                            
         synth = pipelines.ShellStep('synth', input= source,
-                commands = ["cd irfanhassan_skipq2021/Sprint1","pip install aws-cdk.aws_cloudwatch_actions==1.135.0", 
+                commands = ["cd irfanhassan_skipq2021/Sprint2","pip install aws-cdk.aws_cloudwatch_actions==1.135.0", 
                             "pip install -r requirements.txt ","npm install -g aws-cdk","cdk synth" ],
                             primary_output_directory = "irfanhassan_skipq2021/Sprint2/cdk.out"
                             )
