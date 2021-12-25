96ed0b1bcab14859503e6c9ab14db8a10612665a
Muhammad Irfan Hassan <96061669+muhammadskipq2021@users.noreply.github.com>
2021-12-25 22:33:10 +0500

    Delete irfanhassan_skipq2021/Sprint2 directory

--- irfanhassan_skipq2021/Sprint2/irfan_skip_q_project1/irfan_skip_q_project1_stage.py
+++
@@ -1,9 +0,0 @@
-from aws_cdk import core as cdk
-
-from irfan_skip_q_project1.irfan_skip_q_project1_stack import IrfanSkipQProject1Stack
-
-class IrfanSkipqPipelineStage(cdk.Stage):
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-        
-        irfan_stack =  IrfanSkipQProject1Stack(self,'irfanstack')
