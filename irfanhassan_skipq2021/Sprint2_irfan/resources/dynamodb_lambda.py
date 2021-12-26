7a3a2f38f5506285c40fa7815708389513476633
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-26 08:11:20 +0000

    updated

--- irfanhassan_skipq2021/Sprint2_irfan/resources/dynamodb_lambda.py
+++ irfanhassan_skipq2021/Sprint2_irfan/resources/dynamodb_lambda.py
@@ -10,13 +10,13 @@ def lambda_handler(event, context):
     timestamp=message['Timestamp']
     alarm=message['AlarmName']
     if alarm[0]=='B':
-        client.put_item(TableName = "Beta-infraStack-TableCD117FA1-10BTARD7DGVYY",
+        client.put_item(TableName = "BetaStag-irfanskipqstack-irfanhassantable9BB9EE06-Q9YQR7BPL0DL",
         Item={
              'Timestamp':{'S' : message['Timestamp']},
               'Reason':{'S':msg['NewStateReason']
               }})
     else:
-        client.put_item(TableName = "ProdStage-irfanskipqstack-irfanhassantable9BB9EE06-HSG5CFZS1QY0",
+        client.put_item(TableName = "ProdStage-irfanskipqstack-irfanhassantable9BB9EE06-K7N6HU3VLIWO",
         Item={
               'Timestamp':{'S' : message['Timestamp']},
                'Reason':{'S':msg['NewStateReason']
