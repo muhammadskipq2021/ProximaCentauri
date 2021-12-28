c910fd0345fae5a58e98e00b98ce945cfdf52b9c
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-28 11:12:00 +0000

    updated

---
+++ irfanhassan_skipq2021/Sprint2_irfan/cdk.out/asset.aa5210acf861b8ec1fbbb44c8f043df6267b6371a4dc2254a2cbf3c12db84f29/dynamodb_lambda.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+import json
+import constants as constant_
+
+def lambda_handler(event, context):
+    #print(eveny)
+    client = boto3.client('dynamodb')
+    
+######### getiing timssetamp && message details fromm even(alarm)  #########################
+    message = event['Records'][0]['Sns']
+    msg = json.loads(message['Message'])    
+    timestamp=message['Timestamp']
+####### getting name of table ##############################################################
+    TableName = os.getenv('table_name')
+    Item={
+              'Timestamp':{'S' : message['Timestamp']},
+               'Reason':{'S':msg['NewStateReason']}
+        }
+########### puuting Item in dynamo DB ####################################################3
+    client.put_item(TableName,Item)
+        
+
+
+
+    
