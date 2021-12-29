67ce97ddf6b303ba6fe2fbdc52d6ce333710ad33
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-29 19:35:32 +0000

    updatd

--- irfanhassan_skipq2021/Sprint1/cdk.out/asset.a73935d69d38935c8a1a8de426ef8e6629fb3293381f6522e29a4a78bd5136b7/s3bucket_read.py
+++
@@ -1,6 +0,0 @@
-import s3Bucket as s3Bucket
-import constants as constant_
-
-def lambda_handler(event,context):
-    URLS = s3Bucket.s3_bucket_get_data(constant_.bucket,constant_.file_name)
-    print(URLS)
--- irfanhassan_skipq2021/Sprint1/cdk.out/asset.da55c5a2da72476a9a6f817ae21240699b581a12ac2bfd8c5337885d8f7d0551/constants.py
+++
@@ -1,7 +0,0 @@
-URL_NameSpace="irfanhassanSkipQ_WebHealth_Monitor"
-URL_Aailibilty = "URL_Aailibilty"
-URL_Latency = "URL_Latency"
-threshold = [0.31,0.34,0.50,0.23]
-table_name = "Irfandynamodb_Table"
-bucket = "irfanskipq"
-file_name = "URLS.json"
