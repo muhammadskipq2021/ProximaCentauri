30a06b05054244bd3605bc54c096e42ff870d8b3
Muhammad Irfan Hassan <96061669+muhammadskipq2021@users.noreply.github.com>
2021-12-30 15:53:20 +0500

    Delete irfanhassan_skipq2021/leetcode directory

--- irfanhassan_skipq2021/leetcode/Array/sortedSquares.py
+++
@@ -1,9 +0,0 @@
-#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
-class Solution:
-    def sortedSquares(self, nums):
-        nums_sq = [i**2 for i in nums]
-        nums_sq.sort()
-        return nums_sq
-test=Solution()      
-x=[1,3,2,5]
-print(test.sortedSquares(x))
