71e6c3fa7c27061b0653653b4e91b357de5b412a
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-30 11:52:56 +0000

    update

---
+++ irfanhassan_skipq2021/Sprint3_irfan/cdk.out/asset.a82e0766d360ab143e13d3f8937d74ee93f56f44b945b77b45c5f744e745d9ec/s3_dynamodb.py
@@ -0,0 +1,11 @@
+import boto3
+import os
+from s3bucket_read import s3bucket_read as bucket
+
+def lambda_handler(event,context):
+    value = dict()
+    client = boto3.client('dynamodb')
+    list_url=bucket().bucket_as_list()
+    tablename = os.getenv('table_name')#getting table name
+    for url in list_url:
+        client.put_item(TableName= tablename,Item={'URL':{'S' : url}})
