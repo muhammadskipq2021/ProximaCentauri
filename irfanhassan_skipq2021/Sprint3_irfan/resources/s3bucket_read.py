46a699281ed5c03591e55b2099e897c96814aa79
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2021-12-30 15:12:25 +0000

    updated

--- irfanhassan_skipq2021/Sprint3_irfan/resources/s3bucket_read.py
+++ irfanhassan_skipq2021/Sprint3_irfan/resources/s3bucket_read.py
@@ -5,16 +5,15 @@ import boto3
 s3= boto3.client('s3')
 
 class s3bucket_read:
-    def __init__(self):
-        self.Object = boto3.client('s3').get_object(Bucket="irfanskipq",Key="URLS.json")
+    def __init__(self,bucketname, filename):
+        self.Object = boto3.client('s3').get_object(Bucket=bucketname,Key=filename)
     
     def bucket_as_list(self  ):
-        response= s3.get_object(Bucket="irfanskipq",Key="URLS.json")
-        contetnt = response['Body']
-        json_oject = json.loads(contetnt.read())   #get dictionary
-        list_url=[json_oject['link1'],json_oject['link2'],json_oject['link3'],json_oject['link4']]
-        for url in list_url:
-            print(url)
-            print('--------')
         
+        contetnt = self.Object['Body']
+        json_oject = json.loads(contetnt.read())   #get dictionary
+        list_url=list(json_oject.values())
+       # for url in list_url:
+       #     print(url)
+       #     print('--------')
         return list_url
