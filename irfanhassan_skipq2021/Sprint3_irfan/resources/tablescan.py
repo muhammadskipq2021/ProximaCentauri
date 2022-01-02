a9e8cb9d725d26b33be67efa33ab5adc270f7b90
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-02 09:48:20 +0000

    irfan updated code

--- irfanhassan_skipq2021/Sprint3_irfan/resources/tablescan.py
+++ irfanhassan_skipq2021/Sprint3_irfan/resources/tablescan.py
@@ -4,6 +4,6 @@ class tablescan:
     def read_table(self,table_name):
         client = boto3.client('dynamodb')
         table_data = client.scan(TableName=table_name,AttributesToGet=['URL'])
-        url=table_data["Items"]
-        url_list= list(url.values())
+        url_list=table_data["Items"]
+    #    url_list= list(url.values())
         return url_list
