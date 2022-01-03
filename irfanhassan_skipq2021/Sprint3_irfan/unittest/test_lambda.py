a1e2def003910fd336e5b18072d487c130b69a2e
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-03 08:12:13 +0000

    updated

--- irfanhassan_skipq2021/Sprint3_irfan/unittest/test_lambda.py
+++ irfanhassan_skipq2021/Sprint3_irfan/unittest/test_lambda.py
@@ -1,6 +1,9 @@
 import pytest
 from aws_cdk import core 
-
+from sprint3_irfan.sprint3_irfan_stack import Sprint3IrfanStack
 def test_lambda():
-    
-    assert 1==1
+    app = core.App()
+    Sprint2IrfanStack(app, 'Stack')
+    temp = app.synth().get_stack_by_name('Stack').template
+    lambda_function = [resource for resource in temp['Resources'].values()  if resource['Type']=="AWS::Lambda::Function"]
+    assert len(lambda_function)==4
