5f585c750d98e0bd3d4639f23c1c016c53a29e8c
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-03 12:17:23 +0000

    updated code_test

--- irfanhassan_skipq2021/Sprint3_irfan/unittest/code_test.py
+++ irfanhassan_skipq2021/Sprint3_irfan/unittest/code_test.py
@@ -3,7 +3,7 @@ from aws_cdk import core
 #import aws_cdk.assertions as assertions
 from sprint3_irfan.sprint3_irfan_stack import Sprint3IrfanStack
 app=core.App()
-#Sprint3IrfanStack(app, 'Stack')
+Sprint3IrfanStack(app, 'Stack')
 template=app.synth().get_stack_by_name('Stack').template
 ################# TEST 1: Lambda functions #############
 def test_code():
