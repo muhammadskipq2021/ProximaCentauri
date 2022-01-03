ae4883a78185fa25ab87e918e335226ccf79b000
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-03 09:58:03 +0000

    updated tst

--- irfanhassan_skipq2021/Sprint3_irfan/unittest/code_test.py
+++ irfanhassan_skipq2021/Sprint3_irfan/unittest/code_test.py
@@ -1,11 +1,11 @@
 import pytest
 from aws_cdk import core
 #import aws_cdk.assertions as assertions
-from sprint3_irfan.sprint3_irfan_stack import Sprint3IrfanStack
-#app=core.App()
+#from sprint3_irfan.sprint3_irfan_stack import Sprint3IrfanStack
+app=core.App()
 #Sprint3IrfanStack(app, 'Stack')
-#template=app.synth().get_stack_by_name('Stack').template
+template=app.synth().get_stack_by_name('Stack').template
 ################# TEST 1: Lambda functions #############
 def test_code():
-    #functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
+    functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
     assert 4==4
