2b7c1be6d349eb976e966f1d3bb63b8eb577f81a
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-03 21:05:10 +0000

    added Intigration test

--- irfanhassan_skipq2021/Sprint3_irfan/Intigration/apigateway_test.py
+++ irfanhassan_skipq2021/Sprint3_irfan/Intigration/apigateway_test.py
@@ -2,12 +2,12 @@ import datetime
 import pytest
 import requests
 from resources import constants as constants_
-from resources.tablescan import tablescan 
-
+import boto3
 
 def test_apigateway():
-    dbscan=tablescan()
-    url_list1=dbscan.read_table(constants_.url_table)
+    client = boto3.client('dynamodb')
+    table_data = client.scan(TableName=constants_.url_table,AttributesToGet=['URL'])
+    url_list=table_data["Items"]
     api_test = requests.put('https://hwmcjcc01f.execute-api.us-east-2.amazonaws.com/prod/',data = 'www.google.com')
     url_list2=dbscan.read_table(constants_.url_table)
     assert len(url_list1)==len(url_list2)-1
