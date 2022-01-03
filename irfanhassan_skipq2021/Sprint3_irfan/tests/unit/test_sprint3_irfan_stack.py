c169e22ccf6cc98bf7447af9858ee6851ce4e9e0
Ubuntu <ubuntu@ip-172-31-36-136.us-east-2.compute.internal>
2022-01-03 12:20:51 +0000

    test updated

--- irfanhassan_skipq2021/Sprint3_irfan/tests/unit/test_sprint3_irfan_stack.py
+++ irfanhassan_skipq2021/Sprint3_irfan/tests/unit/test_sprint3_irfan_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from sprint2_irfan.sprint2_irfan_stack import Sprint2IrfanStack
+from sprint3_irfan.sprint3_irfan_stack import Sprint3IrfanStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in sprint2_irfan/sprint2_irfan_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = Sprint2IrfanStack(app, "sprint2-irfan")
+    stack = Sprint2IrfanStack(app, "sprint3-irfan")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
